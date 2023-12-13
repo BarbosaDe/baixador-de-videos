aa836b287d9b77f4ab3ab8e5d793abedce806101
BarbosaDe <bellzin3158@gmail.com>
2023-12-13 03:32:30 -0300

    first commit

---
+++ app/downloader/tools.py
@@ -0,0 +1,18 @@
+import pytube, os, time, threading
+
+
+
+
+
+
+class Tools:
+    def filter(self, link):
+        if 'https://www.youtube.com/' in link:
+            pass
+
+        elif 'https://www.instagram.com/' in link:
+            pass
+
+
+    def get_information(self, id):
+        pass
---
+++ app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .downloader import tools
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+
+
+tl = tools.Tools()
+def info(request):
+    if request.method == "GET": return
+
+    link = request.POST['video_id']
+    split = tl._link
+
+    return HttpResponse(split)
+    
+
+    
+"""
+
+https://www.youtube.com/watch?v=ij99kTigTh8&list=RDMMij99kTigTh8&start_radio=1
+
+https://www.instagram.com/p/C0wcKwJtSWDvy0WgympQbTXHn_paJ7XoCWw5i00
+
+
+"""
